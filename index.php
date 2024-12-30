77892f1d14cd68fc37f8986a7b1ae3751f19e6c1
Marouane-bichara <bicharamarouane0@gmail.com>
2024-12-30 09:45:26 +0100

    finshed

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+    include './classes/Administrateur.php';
+    include './classes/Article.php';
+    include './classes/Auteur.php';
+    include './classes//Utilisateur.php';
+
+
+    $admin = new Administrateur("Said", "SaidAbaila@gmail.com" , "admin");
+?>
